0d609d733923e2f208d29d772c5605ddeae2bd7d
RobsonMaestro <robson_maestro@alumni.usp.br>
2021-09-05 20:40:09 -0300

    Instalado a Annotations e Criando Rotas

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
+    /**
+     * @Route("hello_world")
+     */
     public function World(){
         return new Response(
             "<html><body><h1>Hello World</h1></body></html>"
